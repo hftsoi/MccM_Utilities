26172faa81c20c7dbea9bf839c8214d14da00c2e
Alp Akpinar <aakpinar@bu.edu>
2020-10-16 16:17:53 -0500

    Fixup in gridpack paths

--- SUSY_HToAA/create_htoaa_requests.py
+++ SUSY_HToAA/create_htoaa_requests.py
@@ -26,12 +26,12 @@ def create_wh_zh_requests():
     # Gridpack locations 
     gridpack_location_temps = {
         'WH': {
-            2016: '/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc630/13TeV/madgraph/V5_2.6.5/Wh01_M125_Toa01a01_M{__MASS__}_Tobbtautau',
+            2016: '/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc630/13TeV/madgraph/V5_2.6.5/Wh01_M125_Toa01a01_M{__MASS__}_Tobbtautau/v1/Wh01_M125_Toa01a01_M{__MASS__}_Tobbtautau_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz',
             2017: '',
             2018: '',
         },
         'ZH': {
-            2016: '/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc630/13TeV/madgraph/V5_2.6.5/Zh01_M125_Toa01a01_M{__MASS__}_Tobbtautau',
+            2016: '/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc630/13TeV/madgraph/V5_2.6.5/Zh01_M125_Toa01a01_M{__MASS__}_Tobbtautau/v1/Zh01_M125_Toa01a01_M{__MASS__}_Tobbtautau_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz',
             2017: '',
             2018: '',
         }
@@ -76,12 +76,12 @@ def create_vbf_ggh_requests():
     # Gridpack locations 
     gridpack_location_temps = {
         'VBF': {
-            2016: '/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc630/13TeV/madgraph/V5_2.6.5/vbfh01_M125_Toa01a01_M{__MASS__}_Tobbtautau',
+            2016: '/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc630/13TeV/madgraph/V5_2.6.5/vbfh01_M125_Toa01a01_M{__MASS__}_Tobbtautau/v1/vbfh01_M125_Toa01a01_M{__MASS__}_Tobbtautau_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz',
             2017: '',
             2018: ''
         },
         'ggH': {
-            2016: '/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc630/13TeV/madgraph/V5_2.6.5/ggh01_M125_Toa01a01_M{__MASS__}_Tobbtautau',
+            2016: '/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc630/13TeV/madgraph/V5_2.6.5/ggh01_M125_Toa01a01_M{__MASS__}_Tobbtautau/v1/ggh01_M125_Toa01a01_M{__MASS__}_Tobbtautau_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz',
             2017: '',
             2018: ''
         }
